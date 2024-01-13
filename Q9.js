29067342f10286581cc10fca09cc3058ee6c17d0
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 02:52:02 +0500

    NODEJS Assignment Q9
    9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ Q9.js
@@ -0,0 +1,4 @@
+"use strict";
+// 8.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let num = 2020;
+console.log(`my favourite number is ${num}`);
---
+++ Q9.ts
@@ -0,0 +1,4 @@
+// 8.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let num: number = 2020;
+console.log(`my favourite number is ${num}`);
